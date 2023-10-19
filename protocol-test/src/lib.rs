3443306f1cd98e16abb799ebcee5af23c81cbc6e
Peter Donovan <peterdonovan@berkeley.edu>
2023-10-19 11:46:01 -0700

    Add trace file reader.

--- protocol-test/src/lib.rs
+++ protocol-test/src/lib.rs
@@ -1,6 +1,8 @@
-use std::{collections::HashMap, path::PathBuf, process::Command};
+use std::{collections::HashMap, fs::File, path::PathBuf, process::Command};
 
+use csv::{Reader, ReaderBuilder};
 use regex::Regex;
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub struct HookId(String);
@@ -29,24 +31,131 @@ fn get_counts(executable: PathBuf) -> HashMap<HookId, u32> {
   ret
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct TraceRecord {
+  #[serde(rename = "Event")]
+  event: String,
+  #[serde(rename = "Reactor")]
+  reactor: String,
+  #[serde(rename = "Source")]
+  source: i32,
+  #[serde(rename = "Destination")]
+  destination: i32,
+  #[serde(rename = "Elapsed Logical Time")]
+  elapsed_logical_time: i64,
+  #[serde(rename = "Microstep")]
+  microstep: i64,
+  #[serde(rename = "Elapsed Physical Time")]
+  elapsed_physical_time: i64,
+  #[serde(rename = "Trigger")]
+  trigger: String,
+  #[serde(rename = "Extra Delay")]
+  extra_delay: u64,
+}
+
+fn get_traces(executable: PathBuf, scratch: PathBuf) -> HashMap<String, Reader<File>> {
+  Command::new(
+    executable
+      .canonicalize()
+      .expect("failed to resolve executable path")
+      .as_os_str(),
+  )
+  .current_dir(scratch.clone())
+  .output()
+  .expect("failed to execute program to get trace");
+  for entry in scratch
+    .read_dir()
+    .expect("failed to read tracefiles from scratch")
+    .flatten()
+    .filter(|it| it.file_name().to_str().unwrap().ends_with(".lft"))
+  {
+    println!("{entry:?}");
+    Command::new("trace_to_csv")
+      .current_dir(scratch.clone())
+      .arg(entry.file_name())
+      .output()
+      .expect("failed to execute trace_to_csv");
+  }
+  let mut ret = HashMap::new();
+  for entry in scratch
+    .read_dir()
+    .expect("failed to read csvs from scratch")
+    .flatten()
+    .filter(|it| {
+      let s = it.file_name();
+      let s = s.to_str().unwrap();
+      s.ends_with(".csv") && !s.contains("summary.csv")
+    })
+  {
+    ret.insert(
+      PathBuf::from(entry.file_name())
+        .file_name()
+        .expect("file name ends with .., which should be impossible")
+        .to_str()
+        .expect("file name is not valid UTF-8")
+        .to_string(),
+      ReaderBuilder::new()
+        .trim(csv::Trim::All)
+        .from_path(scratch.join(entry.file_name()))
+        .expect("failed to open CSV reader"),
+    );
+  }
+  ret
+}
+
 #[cfg(test)]
 mod tests {
+
   use super::*;
-  use std::path::PathBuf;
+  use std::{fs::DirEntry, path::PathBuf};
 
   fn tests_relpath() -> PathBuf {
     PathBuf::from("../../../../lingua-franca/test/C/bin")
   }
 
-  #[test]
-  fn test_get_counts() {
-    for entry in tests_relpath()
+  fn scratch_relpath() -> PathBuf {
+    let ret = PathBuf::from("./scratch");
+    std::fs::create_dir_all(&ret).expect("failed to create scratch dir");
+    ret
+  }
+
+  fn test_progs() -> impl Iterator<Item = DirEntry> {
+    tests_relpath()
       .read_dir()
       .expect("read_dir call failed")
       .flatten()
-    {
+  }
+
+  #[test]
+  fn test_get_counts() {
+    for entry in test_progs() {
       let counts = get_counts(entry.path());
       println!("{counts:?}");
+    }
+  }
+
+  #[test]
+  fn test_get_traces() {
+    for entry in test_progs() {
+      println!("{entry:?}");
+      let csvs: HashMap<String, Vec<String>> = get_traces(entry.path(), scratch_relpath())
+        .iter_mut()
+        .map(|(name, reader)| {
+          println!("{name:?}");
+          (
+            name.clone(),
+            reader
+              .deserialize()
+              .map(|r| {
+                println!("{r:?}");
+                let r: TraceRecord = r.expect("could not read record");
+                r.event
+              })
+              .collect(),
+          )
+        })
+        .collect();
+      println!("{csvs:?}");
     }
   }
 }
