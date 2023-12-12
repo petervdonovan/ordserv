7c227ce233729e946a8154d6505b915666223614
Peter Donovan <peterdonovan@berkeley.edu>
2023-12-11 16:36:36 -0800

    Increase concurrency limit.

--- go.sh
+++ go.sh
@@ -4,4 +4,4 @@ ulimit -n 200000 || 0
 # echo 200000 > /proc/sys/kernel/threads-max
 time cargo build --release -p protocol-test
 time cargo build --release -p c-ordering-client
-time ./target/release/protocol-test -c 20 -f 60 lf-264/test/C/src/federated/
+time ./target/release/protocol-test -c 50 -f 60 lf-264/test/C/src/federated/
