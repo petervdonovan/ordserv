fa7b3f93d6649de524de1b517b603bbe11b2eda8
Peter Donovan <peterdonovan@berkeley.edu>
2023-12-20 14:17:59 -0800

    Hack to keep n/a's from messing up everything.

--- viz/src/stats.rs
+++ viz/src/stats.rs
@@ -10,7 +10,9 @@ pub struct BasicStats {
 pub type StatProjection = (String, Box<dyn Fn(&BasicStats) -> f64>);
 impl BasicStats {
     pub fn new(data: impl Iterator<Item = f64>) -> Self {
-        let mut data: Vec<_> = data.filter(|it| it.is_finite()).collect();
+        let mut data: Vec<_> = data
+            .map(|it: f64| if it.is_finite() { it } else { 0.0 }) // FIXME: Why 0? This is a hack
+            .collect();
         data.sort_by(|a, b| a.partial_cmp(b).unwrap());
         let len = data.len();
         let mean = data.iter().sum::<f64>() / len as f64;
